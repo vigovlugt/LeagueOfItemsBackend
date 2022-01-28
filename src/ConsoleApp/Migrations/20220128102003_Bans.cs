75ecc2c95316b2a10375a8be326a8669fdd96eb6
Vigo Vlugt <vigovlugt@gmail.com>
2022-01-28 11:28:58 +0100

    Add Bans migrations

---
+++ src/ConsoleApp/Migrations/20220128102003_Bans.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeagueOfItems.ConsoleApp.Migrations
+{
+    public partial class Bans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Bans",
+                table: "ChampionData",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bans",
+                table: "ChampionData");
+        }
+    }
+}
