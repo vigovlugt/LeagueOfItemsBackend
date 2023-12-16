a7b2fca321b26ce25ddbe0f90fa1b6f1e5fbe10e
Vigo Vlugt <vigovlugt@gmail.com>
2023-12-16 15:10:23 +0100

    Update date parsing

--- src/Application/Patches/Queries/GetPatchScheduleQuery.cs
+++ src/Application/Patches/Queries/GetPatchScheduleQuery.cs
@@ -38,13 +38,13 @@ public class GetPatchScheduleQueryHandler : IRequestHandler<GetPatchScheduleQuer
         _logger = logger;
         _patchScheduleUri = new Uri(configuration["Riot:PatchScheduleUrl"]);
     }
-    
+
     public async Task<List<ScheduledPatch>> Handle(GetPatchScheduleQuery request, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Getting Patch Schedule from {PatchScheduleUrl}", _patchScheduleUri.ToString());
         var response = await _client.GetAsync(_patchScheduleUri, cancellationToken);
         response.EnsureSuccessStatusCode();
-        
+
         var json = await JsonSerializer.DeserializeAsync<ZendeskApiResponse>(await response.Content.ReadAsStreamAsync(cancellationToken), new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
@@ -58,14 +58,14 @@ public class GetPatchScheduleQueryHandler : IRequestHandler<GetPatchScheduleQuer
         return tableRows.Select(r =>
         {
             var tds = r.SelectNodes("td");
-            return new ScheduledPatch(tds[0].InnerHtml, ParseDateString(tds[1].InnerHtml));
+            return new ScheduledPatch(tds[0].InnerHtml, ParseDateString(tds[1].SelectSingleNode("span").InnerHtml));
         }).ToList();
     }
 
     private static DateTime ParseDateString(string dateString)
     {
         dateString = dateString.Replace("Sept", "Sep");
-        
+
         return DateTime.Parse(string.Join(" ", dateString.Split(",").Select(x => x.Trim()).Skip(1)));
     }
 }
